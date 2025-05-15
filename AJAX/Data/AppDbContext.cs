cca052338129feed259f4793c01cf068c1598f74
corvert <orvet.priimagi@gmail.com>
2025-05-15 15:20:36 +0300

    models

---
+++ AJAX/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AJAX.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext() { }
+              public AppDBContext(DbContextOptions<AppDBContext> options)
+            : base(options)
+        { }
+        public virtual DbSet<City> Cities { get; set; }
+        public virtual DbSet<Country> Countries { get; set; }
+    }
+}
+
+   
+
