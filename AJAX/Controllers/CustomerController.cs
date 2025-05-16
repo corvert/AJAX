47f9df923a5d149156857a011070d245325127e9
corvert <orvet.priimagi@gmail.com>
2025-05-16 18:43:10 +0300

    update customer controller and edit view

--- AJAX/Controllers/CustomerController.cs
+++ AJAX/Controllers/CustomerController.cs
@@ -63,7 +63,14 @@ namespace AJAX.Controllers
         [HttpPost]
         public IActionResult Edit(Customer customer)
         {
-           
+
+            if (customer.ProfilePhoto != null)
+            {
+                string uniqueFileName = GetProfilePhotoFileName(customer);
+                customer.PhotoUrl = uniqueFileName;
+            }
+         
+
                 _context.Attach(customer);
             _context.Entry(customer).State = EntityState.Modified;
             _context.SaveChanges();
